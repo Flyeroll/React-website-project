0293fe9cbcc6e4a92120ebc840b7b1e07983f6d8
Flyeroll <Flytobefree@mail.ru>
2022-08-04 19:06:31 +0700

    Filter DONE (CLEAR)

--- react-website-project/src/App.js
+++ react-website-project/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Nav from "./Components/nav-component/nav"
+import NavRoute from "./Components/routes/nav/nav.component"
+import FirstRoute from "./Components/routes/nav/main/first.component"
 
 import { Route, Routes} from "react-router-dom"
 
@@ -9,8 +10,9 @@ class App extends Component {
   render() {
     return (
       <Routes>
-          <Route path='/' element={<Nav />}>
+          <Route path='/' element={<NavRoute />}>
             <Route path='default' element={<DefaultRoute/>}></Route>
+            <Route path='first' element={<FirstRoute/>}></Route>
           </Route>
       </Routes>
     );
--- react-website-project/src/Components/main/first/first.jsx
+++ react-website-project/src/Components/main/first/first.jsx
@@ -1,10 +1,71 @@
-import React from "react";
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+
+
+import Data from '../../API/api.json'
+
 
 export default function First(){
 
+    const [initialArray, setinitialArray] = useState(Data)
+    const [filteredArray, setFilteredArray] = useState(Data)
+    
+    const [input, setInput] = useState('')
+
+
+    function filterArray(element) {
+            console.log(input);
+            let elementVar = element
+            setInput((prev) => {
+                return element.target.value
+            })
+
+    }
+
+    useEffect(() => {
+        let copyArr = initialArray
+
+        copyArr = copyArr.map((elem) => {
+            if(elem.name.toLowerCase().includes(input)){
+                return elem
+            } 
+        })
+        
+        let noUndefinedArray = []
+        
+        copyArr.forEach((elem) => {
+            if(elem !== undefined) {
+                noUndefinedArray.push(elem)
+            }
+    })
+
+    setFilteredArray((prev) => {
+        return noUndefinedArray
+    })
+        console.log(noUndefinedArray);
+        // setFilteredArray((prev) => {
+        //     return copyArr
+        // })
+    }, [input])
+
+    
+
+
+    
+
     return (
         <div>
-            "FIRST" PAGE
+            <input type="text"  id="dishesInput" onChange={(element) => filterArray(element) }/>
+             {/* <img src={require("../../../../public/images/fotos/1.1.jpg")} alt="" /> */}
+            {filteredArray.map((prev) => (
+                <div key={prev.id}>
+                    <div key={prev.id}>{prev.name}</div>
+                    <h6>{prev.description}</h6>
+                </ div>
+                ))}
+                    {/* <div >
+                        {input}
+                    </div> */}
         </div>
     )
 }
