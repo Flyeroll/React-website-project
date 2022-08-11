ff7c76e7534daab4c432e4db4791bcda800cd61f
Flyeroll <Flytobefree@mail.ru>
2022-08-11 17:42:08 +0700

    clear canvas DONE (2)

--- react-website-project/src/Components/main/reservation/reservation.jsx
+++ react-website-project/src/Components/main/reservation/reservation.jsx
@@ -4,10 +4,18 @@ import "../reservation/style.css"
 
 export default function Reservation () {
 
-
+    function check () {
+        setTimeout(() => {
+            const drawingCanvas = document.querySelector('.myCanvas');
+            const ctx = drawingCanvas.getContext('2d');
+            console.log(ctx);
+            
+        }, 1000);
+    }
+    check()
     return(
-        <div>
-            RESERVATION PAGE
+        <div className="mainDiv">
+            <canvas className="myCanvas"></canvas>
         </div>
     )
 }
