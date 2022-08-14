a649461e014abeb3b0d6c59c9b93c1b204a56b1b
Flyeroll <Flytobefree@mail.ru>
2022-08-15 03:45:12 +0700

    reservation (6)

--- react-website-project/src/Components/main/reservation/reservation.jsx
+++ react-website-project/src/Components/main/reservation/reservation.jsx
@@ -2,82 +2,35 @@ import React, { useEffect, useState } from "react";
 import "../reservation/style.css"
 
 import { Outlet } from "react-router-dom";
+import { faLineChart } from "@fortawesome/free-solid-svg-icons";
 
 
 export default function Reservation () {
 
-    const [reservedTables, setReservedTables] = useState({
-        1:false,
-        2:false,
-        3:false,
-        4:false,
-        5:false,
-        6:false,
-        7:false,
-        8:false,
-        9:false,
-        10:false
-    })
-    // clear Tables data to display user
-    const [clearReservedTables,setClearReservedTables] = useState('')
 
-    function changeTableStatus(table) {
-        let content = table.target.innerHTML
-        console.log(content);
-        
-        if(table.target.classList.contains('tableFree')) {
-            let classes = table.target.classList
-            classes.add('tableSelected')
-            classes.remove('tableFree')
-            setReservedTables((prev) => {
-                return {...prev, [content]:true}
-            })
-
-
-        } else if (table.target.classList.contains('tableSelected')) {
-            let classes = table.target.classList
-            classes.add('tableFree')
-            classes.remove('tableSelected')
-            setReservedTables((prev) => {
-                return {...prev, [content]:false}
-            })
-        } else {
-            console.log("Table is occupied!");
-        }
+    function moveToFirstStep() {
+        let secondBtn = document.querySelector('.reserveSecondBall')
+        let reserveLine = document.querySelector('.reserveLine')
+        let before = window.getComputedStyle(reserveLine,'::before')
+        console.log(before.width);
+        secondBtn.classList.remove('black')
     }
 
-
-
-    useEffect(() => {
-        let newRawObjTables = reservedTables
-        let newRawArrayTables = Object.values(newRawObjTables)
-        let newClearArrayTables = []
-        for(let i = 0; i < newRawArrayTables.length;i++){
-            if(newRawArrayTables[i] === true) {
-                newClearArrayTables.push(i+1)
-            }
-        }
-
-        setClearReservedTables((prev) => newClearArrayTables)
-        
-    },[reservedTables])
-    
-    useEffect(() => {
-        console.log(clearReservedTables);
-    }, [clearReservedTables])
-    
-    
 
     return(
         <div className="mainDiv">
             <div className="reserveMainWindow">
                 <Outlet />
                 <div className="reserveBtnSection">
-                    <div className="reserveBtn reservBtnBack">back</div>
+                    <div className="reserveBtn reservBtnBack" onClick={() => moveToFirstStep()}>back</div>
                     <div className="reserveBtn reservBtnForwar">continue</div>
                 </div>
                 <div className="reserveShape">
-
+                    <div className="reserveLine">
+                        <div className="reserveFirstBall reserveBall"></div>
+                        <div className="reserveSecondBall reserveBall black"></div>
+                        <div className="reserveThirdBall reserveBall"></div>
+                    </div>
                 </div>
 
             </div>
@@ -105,3 +58,75 @@ export default function Reservation () {
             //         <div className="tableFree table tableNumber" onClick={(table) => changeTableStatus(table)}>10</div>
             //     </div>
             // </div>
+
+
+
+
+
+
+
+     
+
+            /*
+            const [reservedTables, setReservedTables] = useState({
+                1:false,
+                2:false,
+                3:false,
+                4:false,
+                5:false,
+                6:false,
+                7:false,
+                8:false,
+                9:false,
+                10:false
+            })
+            // clear Tables data to display user
+            const [clearReservedTables,setClearReservedTables] = useState('')
+        
+            function changeTableStatus(table) {
+                let content = table.target.innerHTML
+                console.log(content);
+                
+                if(table.target.classList.contains('tableFree')) {
+                    let classes = table.target.classList
+                    classes.add('tableSelected')
+                    classes.remove('tableFree')
+                    setReservedTables((prev) => {
+                        return {...prev, [content]:true}
+                    })
+        
+        
+                } else if (table.target.classList.contains('tableSelected')) {
+                    let classes = table.target.classList
+                    classes.add('tableFree')
+                    classes.remove('tableSelected')
+                    setReservedTables((prev) => {
+                        return {...prev, [content]:false}
+                    })
+                } else {
+                    console.log("Table is occupied!");
+                }
+            }
+        
+        
+        
+            useEffect(() => {
+                let newRawObjTables = reservedTables
+                let newRawArrayTables = Object.values(newRawObjTables)
+                let newClearArrayTables = []
+                for(let i = 0; i < newRawArrayTables.length;i++){
+                    if(newRawArrayTables[i] === true) {
+                        newClearArrayTables.push(i+1)
+                    }
+                }
+        
+                setClearReservedTables((prev) => newClearArrayTables)
+                
+            },[reservedTables])
+            
+            useEffect(() => {
+                console.log(clearReservedTables);
+            }, [clearReservedTables])
+            */
+            
+            
