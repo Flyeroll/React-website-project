a7e51287b2ab1b2c9b6741ab8ef0463ecb1f44aa
Flyeroll <Flytobefree@mail.ru>
2022-08-20 13:44:38 +0700

    reservation (9)

--- react-website-project/src/Components/main/reservation/date_component/date.jsx
+++ react-website-project/src/Components/main/reservation/date_component/date.jsx
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css"
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function DateWindow(){
 
+
+    const [timeArray, setTimeArray] = useState([
+        {time:'10:00', selected:false},
+        {time:'11:00', selected:false},
+        {time:'12:00', selected:false},
+        {time:'13:00', selected:false},
+        {time:'14:00', selected:false},
+        {time:'15:00', selected:false},
+        {time:'16:00', selected:false},
+        {time:'17:00', selected:false},
+        {time:'18:00', selected:false},
+        {time:'19:00', selected:false},
+        {time:'20:00', selected:false},
+    ])
+
+    function changeTimeStatus(elem) {
+        setTimeArray((prev) => {
+            let newArr = prev.map(())
+        })
+        if(!elem.target.classList.contains('selectedSingleTime')) {
+            elem.target.classList.add('selectedSingleTime')
+        } else {
+            elem.target.classList.remove('selectedSingleTime')
+        }
+        console.log(elem.target);
+    }
+
+    function printTime() {
+        return (
+            timeArray.map((elem) => {
+                return <div className="singleTime" onClick={(elem) => changeTimeStatus(elem)}>{elem.time}</div>
+            })
+        )
+    }
+    function changeScroll(elem) {
+        let scrollWindow = document.querySelector('.dateTime')
+        if(elem.target.classList.contains('dateArrowDown')) {
+            scrollWindow.scrollTop += 30
+        } else {
+            scrollWindow.scrollTop -= 30
+        }
+        console.log(timeArray);
+    }
     return (
         <div className="dateMain">
             <div className="dateLeft"></div>
             <div className="dateRight">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam ad excepturi laboriosam sapiente ea porro perferendis doloribus illo, necessitatibus quas nostrum veniam praesentium! Error, eveniet aliquam? Quibusdam, blanditiis accusantium quae maxime tempora dolore rerum laborum. Ipsam, eos. Fuga, delectus eligendi. Itaque saepe est commodi tempora, id pariatur earum ipsam ullam. Voluptates excepturi ratione inventore sit quidem voluptas fugit dicta debitis distinctio illo nulla non obcaecati, ipsam nisi itaque quos ipsum minus laborum! Iure eaque quis neque cumque eos tempore ipsam, suscipit nemo quod aperiam praesentium deleniti sit. Perspiciatis deleniti sint quis asperiores maiores tempore! Amet recusandae fuga quae mollitia aperiam! At sequi expedita, quia ratione similique distinctio perspiciatis quae quas possimus quis mollitia odit enim hic eius dolorum blanditiis alias recusandae qui assumenda. Similique, assumenda. Recusandae accusantium ex, ratione dolores quia natus? Quae voluptatem dicta magnam rem eum voluptatum, esse reprehenderit, perspiciatis quos nostrum dolore, in quia? Ad, error nesciunt? Iste deserunt dolorum quasi dignissimos maiores. Nostrum alias laborum, placeat nihil quibusdam vel delectus quas optio, debitis aliquid inventore cupiditate accusantium obcaecati. Illum perspiciatis sit porro eligendi incidunt eum natus excepturi dignissimos magni pariatur quidem eaque recusandae dicta quia inventore exercitationem, enim nesciunt quisquam in vero at nostrum corrupti. Dolorem.
+                <FontAwesomeIcon icon={faAngleUp} className="dateArrow dateArrowUp" onClick={(elem) => changeScroll(elem)}/>
+                <div className="dateTime">
+                    {printTime()}
+                </div>
+                <FontAwesomeIcon icon={faAngleDown} className="dateArrow dateArrowDown" onClick={(elem) => changeScroll(elem)}/>
             </div>
         </div>
     )
