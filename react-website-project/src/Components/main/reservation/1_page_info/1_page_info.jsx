977af2a26d1f31d05c14eaf26f9daa04ceb7aec5
Flyeroll <Flytobefree@mail.ru>
2022-09-19 22:32:56 +0700

    reservation usersList showed correct (2)

--- react-website-project/src/Components/main/reservation/1_page_info/1_page_info.jsx
+++ react-website-project/src/Components/main/reservation/1_page_info/1_page_info.jsx
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css"
 
 export default function firstPageReserv(props) {
+
+    const[usersList, setUsersList]= useState(props.data3)
+    const[usersListDisplay, setUsersListDisplay] = useState(props.data3)
+
+
+
+    useEffect(() => {
+        let newArr = []
+        for(let i = 0;i < 36; i++){
+            if(usersList[i].counter !== 0){
+                newArr.push(usersList[i])
+            }
+        }
+
+        setUsersListDisplay((prev) => {
+            return newArr
+        })
+        console.log(usersListDisplay);
+    },[usersList])
+
+
+
+    function plusDish(elem) {
+        console.log(elem);
+    }
+
+    function minusDish(elem) {
+        console.log(elem);
+    }
+
+
 
     return (
         <div className="allComponent">
             <h2 className="allComponentTitle">Contact</h2>
             <div className="contactForm">
-                {props.data3[0].name}
+                <div className="contactFormList">
+                    {usersListDisplay.map((elem) => {
+                        return (
+                            <div className="contactFormLine">
+                                <div className="contactFormLineName">{elem.name}</div>
+                                <div className="contactFormLineAmountBox">
+                                    <div className="contactFormLinePlusMinus" onClick={(elem) => minusDish(elem)}>-</div>
+                                    <div className="contactFormLineAmount">{elem.counter}</div>
+                                    <div className="contactFormLinePlusMinus" onClick={(elem) => plusDish(elem)}>+</div>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
