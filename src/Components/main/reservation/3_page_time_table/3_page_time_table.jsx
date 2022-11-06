e697d94e85ac6236e457aebd34a5de0ee592aad2
Flyeroll <Flytobefree@mail.ru>
2022-11-06 15:31:38 +0700

    Styling reservation page 3 DONE

--- src/Components/main/reservation/3_page_time_table/3_page_time_table.jsx
+++ src/Components/main/reservation/3_page_time_table/3_page_time_table.jsx
@@ -4,8 +4,9 @@ import "./style.css"
 export default function thirdPageReserv() {
 
     return (
-        <div>
-            THIRD PAGE STEP
+        <div className="thirdPage">
+            <h1>Thanks for reservation!</h1>
+            <h3>We are waiting for you (DATA) (TIME)</h3>
         </div>
     )
 }
